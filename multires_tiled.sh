2178f37c839b3ffd084d7ed8ba2db8a50e370e9c
ProGamerGov <ProGamerGov@users.noreply.github.com>
2017-01-14 15:28:39 -0700

    Better commenting

--- multires_tiled.sh
+++ multires_tiled.sh
@@ -7,37 +7,39 @@ fi
 
 
 main(){
-	# 1. defines
+	# 1. Defines the content image as a variable
 	input=$1
 	input_file=`basename $input`
 	clean_name="${input_file%.*}"
 	
+	#Defines the style image as a variable
 	style=$2
 	style_dir=`dirname $style`
 	style_file=`basename $style`
 	style_name="${style_file%.*}"
 	
+	#Defines the output directory
 	output="./output"
 	out_file=$output/$input_file
 	
-	# 2. neural-style the original input
+	#Defines the overlap
+	overlap_w=50
+	overlap_h=50
+	
+	# 2. Creates your original styled output. This step will be skipped if you place a previously styled image with the same name 
+	# as your specified "content image", located in your Neural-Style/output/<Styled_Image> directory.
 	if [ ! -s $out_file ] ; then
 		neural_style $input $style $out_file
 	fi
 	
-	original_output_w=`convert $out_file -format "%w" info:`
-	original_output_h=`convert $out_file -format "%h" info:`
-	
-	# 3. tile it
+	# 3. Chop the styled image into 3x3 tiles with the specified overlap value.
 	out_dir=$output/$clean_name
 	mkdir -p $out_dir
-	convert $out_file -crop 3x3+50+50@ +repage +adjoin $out_dir/$clean_name"_%d.png"
-	#To change the crop values, change the +20+20 on line 35, and change the "20" on lines 77,78,99,100. Make sure to use the same value.
+	convert $out_file -crop 3x3+"$overlap_w"+"$overlap_h"@ +repage +adjoin $out_dir/$clean_name"_%d.png"
 	
+	#Finds out the length and width of the first tile as a refrence point for resizing the other tiles.
 	original_tile_w=`convert $out_dir/$clean_name'_0.png' -format "%w" info:`
 	original_tile_h=`convert $out_dir/$clean_name'_0.png' -format "%h" info:`
-
-	
 	
 	 #Resize all tiles to avoid ImageMagick weirdness
 	 convert $out_dir/$clean_name'_0.png' -resize "$original_tile_w"x"$original_tile_h"\! $out_dir/$clean_name'_0.png' 
@@ -56,9 +58,6 @@ main(){
 	mkdir -p $tiles_dir
 	for tile in `ls $out_dir | grep $clean_name"_"[0-9]".png"`
 	do
-		#for i in $( seq 0 8 ); do
-		#	neural_style $out_dir/$clean_name"_$i.png" $style_dir/$style_name"_$i.png" $tiles_dir/$clean_name"_$i.png"
-		#done
 		neural_style_tiled $out_dir/$tile $style $tiles_dir/$tile
 	done
 	
@@ -76,7 +75,6 @@ main(){
 	smush_value_w=`echo $overlap_w $tile_diff_w | awk '{print $1*$2}'`
 	smush_value_h=`echo $overlap_h $tile_diff_h | awk '{print $1*$2}'`
 	
-
 	# 5. feather tiles
 	feathered_dir=$out_dir/feathered
 	mkdir -p $feathered_dir
@@ -87,29 +85,24 @@ main(){
 	done
 	
 	# 7. Smush the feathered tiles together
-
 	convert -background transparent \( $feathered_dir/$clean_name'_0.png' $feathered_dir/$clean_name'_1.png' $feathered_dir/$clean_name'_2.png' +smush -$smush_value_w -background transparent \) \
 		\( $feathered_dir/$clean_name'_3.png' $feathered_dir/$clean_name'_4.png' $feathered_dir/$clean_name'_5.png' +smush -$smush_value_w -background transparent \) \
 		\( $feathered_dir/$clean_name'_6.png' $feathered_dir/$clean_name'_7.png' $feathered_dir/$clean_name'_8.png' +smush -$smush_value_w -background transparent \) \
 		-background none  -background transparent -smush -$smush_value_h  $output/$clean_name.large_feathered.png
 
 	# 8. Smush the non-feathered tiles together
-
 	convert \( $tiles_dir/$clean_name'_0.png' $tiles_dir/$clean_name'_1.png' $tiles_dir/$clean_name'_2.png' +smush -$smush_value_w \) \
 		\( $tiles_dir/$clean_name'_3.png' $tiles_dir/$clean_name'_4.png' $tiles_dir/$clean_name'_5.png' +smush -$smush_value_w \) \
 		\( $tiles_dir/$clean_name'_6.png' $tiles_dir/$clean_name'_7.png' $tiles_dir/$clean_name'_8.png' +smush -$smush_value_w \) \
 		-background none -smush -$smush_value_h  $output/$clean_name.large.png
 
-			
-
-
 	# 8. Combine feathered and un-feathered output images to disguise feathering.
-
 	composite $output/$clean_name.large_feathered.png $output/$clean_name.large.png $output/$clean_name.large_final.png
-
 }
 
 retry=0
+
+#Runs the content image and style image through Neural-Style with your chosen parameters.
 neural_style(){
 	echo "Neural Style Transfering "$1
 	if [ ! -s $3 ]; then
@@ -137,9 +130,9 @@ th neural_style.lua \
 	fi
 	retry=0
 }
-
-
 retry=0
+
+#Runs the tiles through Neural-Style with your chosen parameters. 
 neural_style_tiled(){
 	echo "Neural Style Transfering "$1
 	if [ ! -s $3 ]; then
